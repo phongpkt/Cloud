ac82feff9837f577dc2d9b43bd67f2eeb2ae5e5c
Phong <phamkhacthanhphong@gmail.con>
2022-04-17 09:47:59 +0700

    make it better

--- models/Cars.js
+++ models/Cars.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
-var mongoDB = 'mongodb://localhost:27017/NoSQLBoosterSamples';
+//var mongoDB = 'mongodb://localhost:27017/NoSQLBoosterSamples';
+var mongoDB = 'mongodb://phamkhacthanhphong:phongthanh123@cluster0-shard-00-00.ntq61.mongodb.net:27017,cluster0-shard-00-01.ntq61.mongodb.net:27017,cluster0-shard-00-02.ntq61.mongodb.net:27017/test?replicaSet=atlas-11l1y5-shard-0&ssl=true&authSource=admin'
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 var db = mongoose.connection;
 
